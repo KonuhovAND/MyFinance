cd353bdf213ea062891775ba49d611f261c90081
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-18 17:16:53 +0300

    more secury returnr statment

--- app/models.py
+++ app/models.py
@@ -1,35 +1,37 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Category_spend(models.Model):
     text = models.TextField(max_length=20)
+
     def __str__(self):
         return self.text
+
+
 class Category_income(models.Model):
     text = models.TextField(max_length=20)
+
     def __str__(self):
         return self.text
 
 
-    
 class Spending(models.Model):
-    text = models.TextField(max_length=20)
+    text: models.TextField = models.TextField(max_length=20)
     amount = models.FloatField()
-    category = models.ForeignKey(Category_spend,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category_spend, on_delete=models.CASCADE)
     created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.text +f' amount - {self.amount} catagory - {self.category}'
+        return str(self.text) + f" amount - {self.amount} catagory - {self.category}"
 
-    
+
 class Income(models.Model):
     text = models.TextField(max_length=20)
     amount = models.FloatField()
-    category = models.ForeignKey(Category_income,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category_income, on_delete=models.CASCADE)
     created_at = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.text +f' amount - {self.amount} catagory - {self.category}'
-
-        
+        return str(self.text) + f" amount - {self.amount} catagory - {self.category}"
