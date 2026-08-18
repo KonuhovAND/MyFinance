73635d4ad3f53ba9dbe9aec056780e4166d07fe7
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-18 19:25:47 +0300

    rename Edit.js -> EditCategories.js; update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Add from './pages/Add'
-import Edit from './pages/Edit'
+import EditCategories from './pages/EditCategories';
 import Show_spendings from './pages/Spend.js'
 import Show_incomes from './pages/Income.js'
 import Stats from './pages/Statictics'
@@ -16,14 +16,14 @@ function App() {
         <a className='navigation_bar--element' href='/see_incomes'>See incomes</a>
         <a className='navigation_bar--element' href='/add'>Add</a>
         <a className='navigation_bar--element' href='/categories'>Categories</a>
-        <a className='navigation_bar--element' href='/edit'>Edit categories</a>
+        <a className='navigation_bar--element' href='/edit_categories'>Edit categories</a>
       </nav>
       <Routes>
         <Route path="/" element={<Stats />} />
         <Route path="/see_spendings" element={<Show_spendings />} />
         <Route path="/see_incomes" element={<Show_incomes />} />
         <Route path="/add" element={<Add />} />
-        <Route path="/edit" element={<Edit />} />
+        <Route path="/edit_categories" element={<EditCategories />} />
         <Route path="/" element={<Stats />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
---
+++ frontend/src/pages/EditCategories.js
@@ -0,0 +1,94 @@
+import { useState, useEffect } from "react";
+// {
+//   "meta": {
+//     "limit": 20,
+//     "next": null,
+//     "offset": 0,
+//     "previous": null,
+//     "total_count": 1
+//   },
+//   "objects": [
+//     {
+//       "amount": 100,
+//       "category": "/fake_api/categories_spend/1/",
+//       "created_at": "2026-07-16T10:29:00.898310",
+//       "id": 1,
+//       "resource_uri": "/fake_api/spendings/1/",
+//       "text": "купил булочку"
+//     }
+//   ]
+
+function Panel({ url_categories, category_type }) {
+  const [categories, setCategories] = useState([])
+  const [text, setText] = useState('')
+  const [category, setCategory] = useState('')
+  let selected;
+  let uri;
+
+
+  let loadCategories = () => {
+    fetch(url_categories)
+      .then((r) => r.json())
+      .then((d) => {
+        setCategories(d.objects)
+        setCategory(d.objects[0].resource_uri)
+      })
+  };
+  useEffect(() => { loadCategories(); }, []);
+
+  const handleSelect = (e) => {
+    uri = e.target.value;
+    setCategory(uri);
+    selected = categories.find((c) => c.resource_uri === uri)
+    if (selected) setText(selected.text)
+  }
+  const handleRequest = (e) => {
+
+    e.preventDefault();
+    let data = {
+      category: category,
+      text: text,
+    }
+
+
+    fetch('http://localhost:8000' + category, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then((res) => {
+        loadCategories();
+        setCategory('');
+      }).then(window.location.reload())
+
+  }
+  return (
+    <>
+      <div className='categories'>
+        <p>Change your {category_type} category</p>
+        <form className='fancy-form' onSubmit={handleRequest}>
+          <select className='form-select' value={category} onChange={handleSelect}>
+            {categories.map((item) => (
+              <option value={item.resource_uri} key={item.resource_uri}>{item.text}</option>
+            ))}
+
+          </select>
+          <input className='form-input' type='text' value={text} placeholder='Enter new name of category' onChange={(e) => setText(e.target.value)} />
+          <button className='form-button' type="submit">Save</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+
+function EditCategories() {
+  return (
+    <>
+      <Panel url_categories='http://localhost:8000/fake_api/category_spend/' category_type='spendings' />
+      <Panel url_categories='http://localhost:8000/fake_api/category_income/' category_type='income' />
+    </>
+
+  );
+}
+export default EditCategories;
