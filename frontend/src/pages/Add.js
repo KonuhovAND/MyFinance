23ef328a0fa39ad30f5c4824bddb5e5493134c9c
KonuhovAND <andreykonuhov8@gmail.com>
2026-08-19 17:49:38 +0300

    base for edit.js; handleRequest doesnt work properly; in form person cant change data and no autoloading data from action;

--- frontend/src/pages/Add.js
+++ frontend/src/pages/Add.js
@@ -1,5 +1,4 @@
- import { useState, useEffect } from "react";
-import { data } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 // {
 //   "meta": {
@@ -20,70 +19,71 @@ import { data } from "react-router-dom";
 //     }
 //   ]
 // }
-function Add_operation({url_get_categories_,url_post_operation_,operation_name}){
-    const [amount,setAmount] = useState('')
-    const [category,setCategory] = useState('')
-    const [categories,setCategories] = useState([])
-    const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
-    const [text,setText] = useState('')
+function Add_operation({ url_get_categories_, url_post_operation_, operation_name }) {
+  const [amount, setAmount] = useState('')
+  const [category, setCategory] = useState('')
+  const [categories, setCategories] = useState([])
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+  const [text, setText] = useState('')
 
 
 
-    useEffect(() =>{
-        fetch(url_get_categories_)
-        .then((r) => r.json())
-        .then((d) => {
-         setCategories(d.objects);
-         console.log(d,d.objects);
-         setCategory(d.objects[0].resource_uri);})
-    },[]);
-    const handleRequest = (e) =>{
-        e.preventDefault()
+  useEffect(() => {
+    fetch(url_get_categories_)
+      .then((r) => r.json())
+      .then((d) => {
+        setCategories(d.objects);
+        console.log(d, d.objects);
+        setCategory(d.objects[0].resource_uri);
+      })
+  }, []);
+  const handleRequest = (e) => {
+    e.preventDefault()
 
-        fetch(url_post_operation_,{
-            method: 'POST',
-            headers:{'Content-Type':'application/json'},
-            body: JSON.stringify({
-              amount: amount,
-              category: category,
-              text: text,
-              created_at:date,
-        }
-)
-        }).then((r)=>{
-          if (r.ok){window.location.reload()}
+    fetch(url_post_operation_, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount: amount,
+        category: category,
+        text: text,
+        created_at: date,
+      }
+      )
+    }).then((r) => {
+      if (r.ok) { window.location.reload() }
 
-       }).catch(error=>console.error('Error:',error));
-    }
-    return(
-        <>
-        <div className="categories">
-         <p>Enter {operation_name}</p>
+    }).catch(error => console.error('Error:', error));
+  }
+  return (
+    <>
+      <div className="categories">
+        <p>Enter {operation_name}</p>
         <form className="fancy-form" onSubmit={handleRequest}>
-            <input className="form-input" type="number" value={amount} placeholder="Amount" onChange={(e) => setAmount(e.target.value)} />
-            <input className="form-input" placeholder="Enter text to spending" type='text' name='text' onChange={(e) => setText(e.target.value)}/>
+          <input className="form-input" type="number" value={amount} placeholder="Amount" onChange={(e) => setAmount(e.target.value)} />
+          <input className="form-input" placeholder="Enter text to spending" type='text' name='text' onChange={(e) => setText(e.target.value)} />
 
-            <input type="date" id="date" name="date" value={date} required onChange={(e) => setDate(e.target.value)}/>
+          <input type="date" id="date" name="date" value={date} required onChange={(e) => setDate(e.target.value)} />
 
-            <select className="form-select" value={category} onChange={(e) =>setCategory(e.target.value)}>{categories.map((cat) => (<option value={cat.resource_uri} key={cat.id}>{cat.text}</option>))}</select>
+          <select className="form-select" value={category} onChange={(e) => setCategory(e.target.value)}>{categories.map((cat) => (<option value={cat.resource_uri} key={cat.id}>{cat.text}</option>))}</select>
 
-            <button className='form-button'  type="submit">Save</button>
+          <button className='form-button' type="submit">Save</button>
         </form>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 
 
 }
 
 
-function App_add(){
-    return(
-        <>
-            <Add_operation url_get_categories_='http://localhost:8000/fake_api/category_spend/' url_post_operation_='http://localhost:8000/fake_api/spending/' operation_name='spending'/>
-            <Add_operation url_get_categories_='http://localhost:8000/fake_api/category_income/' url_post_operation_='http://localhost:8000/fake_api/income/' operation_name='income'/>
-        </>
-    );
+function App_add() {
+  return (
+    <>
+      <Add_operation url_get_categories_='http://localhost:8000/fake_api/category_spend/' url_post_operation_='http://localhost:8000/fake_api/spending/' operation_name='spending' />
+      <Add_operation url_get_categories_='http://localhost:8000/fake_api/category_income/' url_post_operation_='http://localhost:8000/fake_api/income/' operation_name='income' />
+    </>
+  );
 
 }
 
--- frontend/src/pages/Edit.js
+++ frontend/src/pages/Edit.js
@@ -4,18 +4,89 @@
   1. слайдер через который можно выбрать что мы хотим изменить
   2. кнопка удаленния 
  */
-// import { useEffect, useState } from "react";
-// function Panel({ get_url_act, get_url_cat, name }) {
-//
-//   return (
-//     <>
-//       <div>
-//
-//       </div>
-//     </>
-//   )
-// }
-function Edit() {
+import { useEffect, useState } from "react";
+function Panel({ get_url_act, get_url_cat, name }) {
+  const [categories, setCategories] = useState({})
+  const [actions, setActions] = useState([])
+  const [action, setAction] = useState([])
+  const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState('')
+  const [text, setText] = useState('')
+  const [date, setDate] = useState(new Date().toString().split('T')[0])
+  let uri;
 
+  useEffect(() => {
+    fetch(get_url_cat)
+      .then((r) => r.json())
+      .then((d) => {
+        const catMap = {}
+        d.objects.forEach((element) => {
+          catMap[element.resource_uri] = element.text
+          catMap['id'] = element.id
+        });
+        setCategories(catMap)
+      })
+    fetch(get_url_act)
+      .then((r) => r.json())
+      .then((d) => {
+        setActions(d.objects)
+
+      })
+  }, [get_url_cat, get_url_act])
+
+  const handleRequest = (e) => {
+
+    e.preventDefault()
+    fetch('http://localhost:8000' + uri, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount: amount,
+        category: category,
+        text: text,
+        created_at: date,
+      })
+        .then((r) => {
+          if (r.ok) { window.location.reload() }
+        })
+        .catch(error => console.error(error))
+    }
+    )
+  }
+  return (
+    <>
+      <div>
+        <form className="fancy-form" onSubmit={handleRequest}>
+          <select>
+            {actions.map((item) => (
+              <option value={item.id} key={item.id} onChange={(e) => { setAction(e.target.value); uri = item.resource_uri }}>{item.created_at.toString().split('T')[0]} {item.text} {item.id} {categories[item.category]}</option>
+            ))}
+          </select>
+          <input className="form-input" type="number" value={action.amount} placeholder="Amount" onChange={(e) => setAmount(e.target.value)} />
+          <input className="form-input" placeholder="Enter text to spending" type='text' name='text' onChange={(e) => setText(e.target.value)} />
+
+          <input type="date" id="date" name="date" value={date} required onChange={(e) => setDate(e.target.value)} />
+
+          <select className="form-select" value={category} onChange={(e) => setCategory(e.target.value)}>
+            {Object.entries(categories).map(([key, item]) => (
+              <option key={key} value={key}>{item}</option>
+            ))
+            }
+          </select>
+
+          <button className='form-button' type="submit">Save</button>
+
+
+        </form>
+      </div>
+    </>
+  )
+}
+function Edit() {
+  return (
+    <>
+      <Panel get_url_act='http://localhost:8000/fake_api/spending/' get_url_cat='http://localhost:8000/fake_api/category_spend/' name='spending' />
+    </>
+  )
 }
 export default Edit;
